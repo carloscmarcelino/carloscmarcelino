a7b4edaa3e9248fcbbcc0bb6a16c512ab2284c29
carlos <76910462+carloscmarcelino@users.noreply.github.com>
2024-09-28 11:11:58 -0300

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import '../styles/globals.css';
+
+import type { Metadata } from 'next';
+import { Montserrat } from 'next/font/google';
+import { ReactNode } from 'react';
+
+import { GlobalProvider } from '@/providers';
+
+const montserratFont = Montserrat({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-sans',
+});
+
+export const metadata: Metadata = {
+  title: 'Next Leap',
+  description: 'Next.js boilerplate made by Loomi',
+};
+
+type RootLayoutProps = Readonly<{
+  children: ReactNode;
+}>;
+
+const RootLayout = ({ children }: RootLayoutProps) => (
+  <html lang="pt_BR" className={montserratFont.variable}>
+    <body className={montserratFont.className}>
+      <GlobalProvider>{children}</GlobalProvider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
